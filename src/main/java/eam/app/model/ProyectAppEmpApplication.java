19e3d3bfadcd89f332540b6bde94fe097c6978bc
Bryan Benavides <104537354+Rainydata@users.noreply.github.com>
2024-03-03 22:32:32 -0500

    project created with its dependencies

---
+++ src/main/java/eam/app/model/ProyectAppEmpApplication.java
@@ -0,0 +1,13 @@
+package eam.app.model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectAppEmpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectAppEmpApplication.class, args);
+	}
+
+}
